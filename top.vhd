402319336a626ad95eae24f0a450d6b4c4b59be9
Marco Mese <mese@na.infn.it>
2026-01-30 14:00:15 +0100

    Single ended triggers and removed debug and exttrig and pulse

--- top.vhd
+++ top.vhd
@@ -37,8 +37,8 @@ entity radioroc_fw is
 		sc_NORT1      : in    std_logic;
 		sc_NORTQ      : in    std_logic;
 
-		T_p           : in std_logic_vector(63 downto 0);
-		T_n           : in std_logic_vector(63 downto 0);
+		T_1           : in std_logic_vector(63 downto 0);
+		T_2           : in std_logic_vector(63 downto 0);
 
 		ADC_SCKHG_p   : out std_logic;
 		ADC_SCKHG_n   : out std_logic;
@@ -97,7 +97,7 @@ architecture arch of radioroc_fw is
 
     -- LVDS
 	signal ADC_SCKHG, ADC_SCKLG, ADC_HG, ADC_LG : std_logic;
-	signal T, tEdge                                       : std_logic_vector(63 downto 0);
+	signal tEdge1, tEdge2 : std_logic_vector(63 downto 0);
 	-- Clock and reset
 	signal reset, locked_1, locked_2, locked_3                               : std_logic;
 	signal clk_2M, clk_10M, clk_50M, clk_100M, clk_200M, clkN_100M, clkN_200M : std_logic;
@@ -242,42 +242,57 @@ nCMOS <= '1';
 
 n_reset_i2c <= en_clki2c and npwr_reset;
 
-dbgOR <= dbgFF(3) or dbgFF(2) or dbgFF(1) or dbgFF(0);
+dbgOR <= '0';--dbgFF(3) or dbgFF(2) or dbgFF(1) or dbgFF(0);
+dbgFF <= (others => '0');
 
-dbgFFInst: process(reset, clk_200M)
-begin
-    if rising_edge(clk_200M) then
-        if reset = '1' then
-            dbgFF <= (others => '0');
-        else
-            dbgFF <= sc_outd_probe & sc_NORT2 & sc_NORT1 & sc_NORTQ;
-        end if;
-    end if;
-end process;
+--dbgFFInst: process(reset, clk_200M)
+--begin
+--    if rising_edge(clk_200M) then
+--        if reset = '1' then
+--            dbgFF <= (others => '0');
+--        else
+--            dbgFF <= sc_outd_probe & sc_NORT2 & sc_NORT1 & sc_NORTQ;
+--        end if;
+--    end if;
+--end process;
 
-inTrgSync: entity work.trgSync
+inTrg1Sync: entity work.trgSync
 generic map(
-    trgNum => t'length
+    trgNum => T_1'length
 )
 port map(
     clk  => clk_100M,
     rst  => reset,
-    tIn  => T,
-    tOut => tEdge
+    tIn  => T_1,
+    tOut => tEdge1
 );
 
-extTrgSync: process(reset, clk_200M)
-begin
-    if rising_edge(clk_200M) then
-        if reset = '1' then
-            extTrgFF  <= '0';
-            extTrgSig <= '0';
-        else
-            extTrgFF  <= extTrg;
-            extTrgSig <= extTrgFF;
-        end if;
-    end if;
-end process;
+inTrg2Sync: entity work.trgSync
+generic map(
+    trgNum => T_2'length
+)
+port map(
+    clk  => clk_100M,
+    rst  => reset,
+    tIn  => T_2,
+    tOut => tEdge2
+);
+
+extTrgFF  <= '0';
+extTrgSig <= '0';
+
+--extTrgSync: process(reset, clk_200M)
+--begin
+--    if rising_edge(clk_200M) then
+--        if reset = '1' then
+--            extTrgFF  <= '0';
+--            extTrgSig <= '0';
+--        else
+--            extTrgFF  <= extTrg;
+--            extTrgSig <= extTrgFF;
+--        end if;
+--    end if;
+--end process;
 
 IOs : entity xil_defaultlib.IO
 port map(
@@ -285,8 +300,6 @@ port map(
     ADC_SCKLG => ADC_SCKLG,
     ADC_HG    => ADC_HG,
     ADC_LG    => ADC_LG,
-    T_p       => T_p,
-    T_n       => T_n,
     ADC_SCKHG_p => ADC_SCKHG_p,
     ADC_SCKHG_n => ADC_SCKHG_n,
     ADC_SCKLG_p => ADC_SCKLG_p,
@@ -295,7 +308,6 @@ port map(
     ADC_HG_n    => ADC_HG_n,
     ADC_LG_p => ADC_LG_p,
     ADC_LG_n => ADC_LG_n,
-    T       => T,
     readRq   => readRq,
     readRq_p => readRq_p,
     readRq_n => readRq_n,
@@ -352,7 +364,7 @@ port map(
     I => clk_10M
 );
 
-dbgOut <= dout_acq(dout_acq'left downto 1) & dbgOR;
+dbgOut <= (others => '0');--dout_acq(dout_acq'left downto 1) & dbgOR;
 
 adc: entity xil_defaultlib.adc
 port map(
@@ -366,7 +378,7 @@ port map(
     NORT2 	 => sc_NORT2,
     NORTQ    => sc_NORTQ,
     nb_acq   => nb_acq,
-    t		 => t,
+    t		 => T_1,
     sel_adc => sel_adc,
     rd_en 	 => rd_acq,
     dout 	 => dout_acq,
@@ -392,7 +404,7 @@ port map(
 
 trgSamplerInst: entity work.trgSamplerCtrl
 generic map(
-    trgNum        => T'length,
+    trgNum        => T_1'length,
     nSAfterTrgDef => 16
 )
 port map(
@@ -400,7 +412,7 @@ port map(
     clkTmr     => clk_100M,
     rst        => reset,
     evtTrigger => evtTrigger,
-    trgIn      => tEdge,
+    trgIn      => tEdge1,
     devExec    => devExec,
     devId      => devId,
     devRw      => devRw,
@@ -417,13 +429,13 @@ port map(
 
 rateMetersInst: entity work.rateMetersCtrl
 generic map(
-    trgNum     => T'length
+    trgNum     => T_1'length
 )
 port map(
     clk        => clk_100M,
     clkTmr     => clk_100M,
     rst        => reset,
-    trgIn      => tEdge,
+    trgIn      => tEdge1,
     devExec    => devExec,
     devId      => devId,
     devRw      => devRw,
--- weeroc/IO.vhd
+++ weeroc/IO.vhd
@@ -11,8 +11,6 @@ entity IO is
     ADC_SCKLG : in std_logic;
     ADC_HG    : out std_logic;
     ADC_LG    : out std_logic;
-    T_p     : in std_logic_vector(63 downto 0);
-    T_n : in std_logic_vector(63 downto 0);
     ADC_SCKHG_p : out std_logic;
     ADC_SCKHG_n : out std_logic;
     ADC_SCKLG_p : out std_logic;
@@ -21,7 +19,6 @@ entity IO is
     ADC_HG_n    : in std_logic;
     ADC_LG_p : in std_logic;
     ADC_LG_n : in std_logic;
-    T : out std_logic_vector(63 downto 0);
     readRq   : in std_logic;
     readRq_p : out std_logic;
     readRq_n : out std_logic;
@@ -45,21 +42,6 @@ architecture Behavioral of IO is
 signal tSig : std_logic_vector(63 downto 0);
 
 begin
-
-TRIG : for I in 0 to 63 generate
-
-    T(I) <= not tSig(I);
-
-   IBUF : IBUFDS
-   Generic map (
-   DIFF_TERM => TRUE
-   )
-   Port map (
-   O => tSig(I),--T(I),--O => T(I) pour radio OB => T(I) pour psiroc
-   I => T_p(I),
-   IB => T_n(I)
-   );
-end generate;
 
 OBUF2 : OBUFDS
 Port map (
