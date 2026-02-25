0d351f26c113b377ce14c06a7eca3c46e8b80245
Marco Mese <60669466+marcomese@users.noreply.github.com>
2026-02-25 18:06:34 +0100

    Update SPISlave.vhd
    return to edgeDet

--- SPISlave.vhd
+++ SPISlave.vhd
@@ -74,9 +74,7 @@ port(
 end component;
 
 signal sclkRise,
-       sclkFF,
        sclkFall,
-       csFF,
        csRise,
        csFall,
        txPres,
@@ -92,12 +90,6 @@ signal buffIn,
 
 begin
 
-sclkRise <= sclk and not sclkFF;
-
-sclkFall <= not sclk and sclkFF;
-
-csRise   <= cs and not csFF;
-
 miso       <= buffOut(7);
 
 lastBit    <= bitCount(bitCount'left);
@@ -107,19 +99,6 @@ loadBuff   <= and_reduce(std_logic_vector(bitCount(2 downto 0)));
 loadRxFifo <= lastBit and rxEna;
 
 loadTxFifo <= lastBit;
-
-sclkCsFF: process(clk, rst)
-begin
-    if rising_edge(clk) then
-        if rst = '1' then
-            sclkFF <= '0';
-            csFF   <= '0';
-        else
-            sclkFF <= sclk;
-            csFF   <= cs;
-        end if;
-    end if;
-end process;
 
 txPresInst: process(clk, rst, txPres)
 begin
@@ -131,6 +110,42 @@ begin
         end if;
     end if;
 end process;
+
+sclkRiseInst: entity work.edgeDetector
+generic map(
+    clockEdge => "falling",
+    edge      => "rising"
+)
+port map(
+    clk       => clk,
+    rst       => rst,
+    signalIn  => sclk,
+    signalOut => sclkRise
+);
+
+sclkFallInst: entity work.edgeDetector
+generic map(
+    clockEdge => "falling",
+    edge      => "falling"
+)
+port map(
+    clk       => clk,
+    rst       => rst,
+    signalIn  => sclk,
+    signalOut => sclkFall
+);
+
+csRiseInst: entity work.edgeDetector
+generic map(
+    clockEdge => "falling",
+    edge      => "rising"
+)
+port map(
+    clk       => clk,
+    rst       => rst,
+    signalIn  => cs,
+    signalOut => csRise
+);
 
 rxEnaProc: process(clk, rst, csRise, rx_ena)
 begin
