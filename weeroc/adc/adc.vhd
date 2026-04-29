6aa896fb7460a3451149cefb914396a49a4559a4
Marco Mese <60669466+marcomese@users.noreply.github.com>
2026-04-29 20:10:48 +0200

    Update adc.vhd
    restored fifo generator, xpm apparently was not working. still not working with fif ogenerator

--- weeroc/adc/adc.vhd
+++ weeroc/adc/adc.vhd
@@ -46,6 +46,20 @@ end adc;
 
 architecture Behavioral of adc is
 
+    COMPONENT fifo_acq
+      PORT (
+        clk : IN STD_LOGIC;
+        srst : IN STD_LOGIC;
+        din : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        wr_en : IN STD_LOGIC;
+        rd_en : IN STD_LOGIC;
+        dout : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        full : OUT STD_LOGIC;
+        empty : OUT STD_LOGIC;
+        valid : OUT STD_LOGIC;
+        rd_data_count : OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+      );
+    END COMPONENT;
 
 	type state_t is (init, idle, wait_hold, rst_cpt, wait_conv, asrt_rd_high, asrt_rd_low, nxt, read_asic, start_conv, end_conv, read_adc, end_read_adc, write_fifo, finish);
 	signal current_state, next_state : state_t;
@@ -128,34 +142,20 @@ port map(
 
 	sdo_hglg <= sdo_hg_des & sdo_lg_des;
 
-    adcFifo: xpm_fifo_sync
-    generic map(
-        FIFO_WRITE_DEPTH    => 16384,
-        READ_DATA_WIDTH     => 8,
-        WRITE_DATA_WIDTH    => 32,
-        RD_DATA_COUNT_WIDTH => 17,
-        READ_MODE           => "std",
-        USE_ADV_FEATURES    => "1400",
-        FIFO_MEMORY_TYPE    => "block"
-    )
-    port map(
-        wr_clk        => clk_200M,
-        rst           => locRst,
-        din           => din_l,
-        wr_en         => wenSync,
+	ff : fifo_acq
+	port map (
+		srst    => locRst,
+		clk => clk_200M,
+		din    => din_l,
+		wr_en  => wenSync,
+		rd_en  => rd_en,
+		dout   => dout,
+		full   => open,
+		empty  => empty_acq,
+		valid => rdValidSig,
+		rd_data_count => rd_data_count_acq
+	);
 
-        dout          => dout,
-        rd_en         => rd_en,
-        empty         => empty_acq,
-        data_valid    => rdValidSig,
-        rd_data_count => rd_data_count_acq,
-        full          => open,
-        wr_rst_busy   => open,
-        rd_rst_busy   => open,
-        sleep         => '0',
-        injectdbiterr => '0',
-        injectsbiterr => '0'
-    );
 
    wenSyncProc: process(clk_200M)
    begin
